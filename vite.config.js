2156f20736637d2d2153d4422ee1baa15c925ffd
Superbia133 <egriffeth@byupathway.edu>
2025-05-18 11:40:21 -0700

    fixed

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { resolve } from 'path';
+
+export default {
+  build: {
+    outDir: '../dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+        cart: resolve(__dirname, 'src/cart/index.html'),
+        checkout: resolve(__dirname, 'src/checkout/index.html'),
+        product: resolve(__dirname, 'src/product_pages/index.html'),
+      },
+    },
+  },
+};
